d14ed0e950aadb8f858dec0df09315c8dc0ed385
TanyaS <sta-88@mail.ru>
2018-01-31 11:54:28 +0300

    function for different digits in number

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <algorithm>
 #include <ctime>
+#include <cmath>
 #include <vector>
 
 using namespace std;
@@ -24,13 +25,32 @@ vector<int> NumberToVector (int n){
 	}
 	return v;
 }
+int CheckAndChange (int n){
+	vector<int> v = NumberToVector(n);	
+	for (size_t i = 0; i < v.size(); i++ ){
+		if ( count(v.begin(), v.end(), v[i]) > 1 ){
+			v[i] = 1 + rand() % 9;
+			i = -1;
+		}
+	}
+	n = 0;
+	for (size_t i=v.size()-1; i>=1; i--){
+		n += v[i]*pow(10,i);
+	}
+	n += v[0];
+	return n;
+}
+
 
 int main(){
 
 	int X = RandFunc();
-	vector<int> X_digits = NumberToVector(X);
-	cout << X << endl;
-	// PrintV(X_digits);
+	vector<int> X_digits;
+	// cout << X << endl;
+	X = CheckAndChange(X);
+	// cout << X << endl;
+	X_digits = NumberToVector(X);
+
 	int Y = 0;
 	vector<int> Y_digits;
 	int cows=0, bulls=0;	
@@ -47,7 +67,7 @@ int main(){
 		// PrintV(Y_digits);
 
 		cows=0, bulls=0;
-		for( int i=0; i<Y_digits.size(); i++ ){
+		for( size_t i=0; i<Y_digits.size(); i++ ){
 		    // cout << i <<endl;
 			if( Y_digits[i] == X_digits[i] ){
 				bulls++;
